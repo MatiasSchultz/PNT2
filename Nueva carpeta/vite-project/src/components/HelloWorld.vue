0c8d212d9def1ed4952323209d57daa944937010
MatiasSchultz <matiisch1@gmail.com>
2023-04-26 11:33:16 -0300

    mensaje

---
+++ Nueva carpeta/vite-project/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="personaje in personajes" :key="personaje.id">{{ personaje.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      personajes: []
+    };
+  },
+  mounted() {
+    fetch('https://rickandmortyapi.com/api/character/1,2,3,4,5')
+      .then(response => response.json())
+      .then(personajes => {
+        this.personajes = personajes;
+      });
+  }
+};
+</script>
+
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
